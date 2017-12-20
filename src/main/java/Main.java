a45524364a8de51bba3f55db2fac52b3b7d2bb4b
TTvanWillegen <tobyvanwillegen@hotmail.com>
2017-12-20 23:15:55 +0100

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.core.exceptions.RateLimitedException;
 public class Main {
     public static void main(String[] args) {
         try {
+            System.out.println(args[0]);
             JDA api = new JDABuilder(AccountType.BOT)
                           .setToken(args[0])
                           .setGame(Game.watching("you debugging."))
